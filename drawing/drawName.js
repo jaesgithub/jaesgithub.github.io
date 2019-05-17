6e8d761800cde3dd549e3a74898acd29d0679937
jaesgithub <jaewshin04@gmail.com>
2019-05-17 16:59:55 -0700

    going into java

---
+++ drawing/drawName.js
@@ -0,0 +1,52 @@
+function drawName() {
+    ctx.fillStyle = "black"; // Changes the fill colour to the value indicated.
+    // ctx.fillRect(25*UNIT,10*UNIT,5*UNIT,5*UNIT);
+    // ctx.fillRect(50*UNIT,10*UNIT,5*UNIT,5*UNIT);
+    // ctx.fillRect(25*UNIT,30*UNIT,30*UNIT,5*UNIT);
+    
+    // ctx.fillRect(8*UNIT,1*UNIT,1*UNIT,1*UNIT);
+    // ctx.fillRect(6*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    // ctx.fillRect(5*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    // ctx.fillRect(4*UNIT,5*UNIT,1*UNIT,1*UNIT);
+    // ctx.fillRect(5*UNIT,6*UNIT,7*UNIT,1*UNIT);
+    // ctx.fillRect(12*UNIT,5*UNIT,1*UNIT,1*UNIT);
+    // ctx.fillRect(11*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    // ctx.fillRect(9*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    
+    ctx.fillRect(0*UNIT,0*UNIT,4*UNIT,1*UNIT);
+    ctx.fillRect(3*UNIT,1*UNIT,1*UNIT,4*UNIT);
+    ctx.fillRect(1*UNIT,5*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(0*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(5*UNIT,1*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(7*UNIT,2*UNIT,1*UNIT,4*UNIT);
+    ctx.fillRect(5*UNIT,3*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(4*UNIT,4*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(5*UNIT,5*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(9*UNIT,3*UNIT,3*UNIT,1*UNIT);
+    ctx.fillRect(11*UNIT,2*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(9*UNIT,1*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(8*UNIT,2*UNIT,1*UNIT,3*UNIT);
+    ctx.fillRect(9*UNIT,5*UNIT,3*UNIT,1*UNIT);
+    ctx.fillRect(12*UNIT,2*UNIT,1*UNIT,3*UNIT);
+    ctx.fillRect(13*UNIT,5*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(14*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(15*UNIT,5*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(16*UNIT,2*UNIT,1*UNIT,3*UNIT);
+    ctx.fillRect(18*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(17*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(18*UNIT,5*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(20*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(22*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(21*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(22*UNIT,5*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(24*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(25*UNIT,2*UNIT,1*UNIT,4*UNIT);
+    ctx.fillRect(26*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(28*UNIT,3*UNIT,1*UNIT,3*UNIT);
+    ctx.fillRect(30*UNIT,2*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(29*UNIT,3*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(30*UNIT,5*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(32*UNIT,2*UNIT,1*UNIT,6*UNIT);
+    ctx.fillRect(30*UNIT,8*UNIT,2*UNIT,1*UNIT);
+    // Homework: Write my name on the convas 
+}
